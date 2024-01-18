ef141b6949251e7426190635bf7b7e0d5135b416
Iota <enoua5@gmail.com>
2024-01-17 23:11:57 -0700

    Add text explaining years

--- src/app/comet-of-change/page.tsx
+++ src/app/comet-of-change/page.tsx
@@ -4,6 +4,10 @@ export default function Page() {
 
         <h1>Comet of Change</h1>
         <h2 id="years">Years</h2>
-    
+        <p>
+            Years are measured by the appearance of the comet, with each appearance marking the first day of the year.
+            Time before the appearance of the first comet is referred to as BC for "Before Comet".
+            Subsequent years are referred to as "First Comet", "Second Comet", etc.
+        </p>
     </>)
 }
